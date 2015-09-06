0bb282f2149a0d53c1542a4336f5e4f0f77f9f81
Kiana Woodward <kianawoodward@gmail.com>
2015-09-05 17:24:45 -0700

    Created an initial version of the repo.

---
+++ front-end-lesson-master/resources/javascript.js
@@ -0,0 +1,14 @@
+
+		$(document).ready(function() {
+			$("#copyright").click(function(){ 
+  				$(this).css('color','purple'); 
+             });
+		});
+
+  /* Add code from the next steps here */
+
+
+		// Your Javascript will go here!
+// document.getElementById('copyright').onclick = function () { 
+   // this.style.color = 'purple';
+// };
